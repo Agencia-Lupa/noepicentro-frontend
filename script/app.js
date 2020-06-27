03ce76de2f17e5437d5d89e240d310d817d02397
Vinicius Sueiro <hi@vsueiro.com>
2020-06-27 19:50:30 -0300

    Adds structure for story and poster download page

--- script/app.js
+++ script/app.js
@@ -9,3 +9,15 @@ const app = {
 }
 
 app.initialize()
+
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
