868aaa0123952f931f6b2bac818a96f094d24cf2
Vinicius Sueiro <hi@vsueiro.com>
2020-06-28 14:15:45 -0300

    Adds basic object-oriented JavaScript structure

--- script/app.js
+++ script/app.js
@@ -1,23 +1,108 @@
 const app = {
 
+  pages : {
+
+    current : '',
+
+    open : function( name ) {
+
+    },
+
+    close : function( name ) {
+
+    },
+
+    initialize : function() {
+
+    }
+
+  },
+
+  cover : {
+
+    initialize : function() {
+
+    }
+
+  },
+
+  main : {
+
+    initialize : function() {
+
+    }
+
+  },
+
+  search : {
+
+    initialize : function() {
+
+    }
+
+  },
+
+  story : {
+
+    carousel : {
+
+      instance : undefined,
+
+      container : '.swiper-container',
+
+      options : {
+
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'progressbar',
+        },
+
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+
+      },
+
+      initialize : function() {
+
+        app.story.carousel.instance = new Swiper(
+          app.story.carousel.container,
+          app.story.carousel.options
+        )
+
+      }
+
+    },
+
+    initialize : function() {
+
+      app.story.carousel.initialize()
+
+    }
+
+  },
+
+  poster : {
+
+    initialize : function() {
+
+
+    }
+
+  },
+
   initialize : function() {
 
-    console.log( 'initialized' )
+    app.pages.initialize()
+    app.cover.initialize()
+    app.main.initialize()
+    app.search.initialize()
+    app.story.initialize()
+    app.poster.initialize()
 
   }
 
 }
 
 app.initialize()
-
-
-var swiper = new Swiper('.swiper-container', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
