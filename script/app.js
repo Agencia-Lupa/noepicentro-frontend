b4d64a99cb0a3af8609e176fd0cf592b267db176
Vinicius Sueiro <hi@vsueiro.com>
2020-06-28 21:03:44 -0300

    Adds search input interactions (without geocoder)

--- script/app.js
+++ script/app.js
@@ -1,3 +1,348 @@
+// Bairro, Cidade, UF — CEP
+
+
+// span feature.text + ' ' + feature.address
+// span Bairro, Cidade, UF — CEP
+
+const sample = {
+  "type": "FeatureCollection",
+  "query": [
+    "rua",
+    "turiassu",
+    "1347"
+  ],
+  "features": [
+    {
+      "id": "address.701811296",
+      "type": "Feature",
+      "place_type": [
+        "address"
+      ],
+      "relevance": 1,
+      "properties": {
+        "accuracy": "point"
+      },
+      "text_pt-br": "Rua Turiassu",
+      "place_name_pt-br": "Rua Turiassu 1347, São Paulo - São Paulo, 05005, Brasil",
+      "text": "Rua Turiassu",
+      "place_name": "Rua Turiassu 1347, São Paulo - São Paulo, 05005, Brasil",
+      "center": [
+        -46.675731,
+        -23.530654
+      ],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -46.675731,
+          -23.530654
+        ]
+      },
+      "address": "1347",
+      "context": [
+        {
+          "id": "locality.16135275663286880",
+          "wikidata": "Q10348536",
+          "text_pt-br": "Perdizes",
+          "language_pt-br": "pt",
+          "text": "Perdizes",
+          "language": "pt"
+        },
+        {
+          "id": "postcode.14659260658953980",
+          "text_pt-br": "05005",
+          "text": "05005"
+        },
+        {
+          "id": "place.9128493891838220",
+          "wikidata": "Q174",
+          "text_pt-br": "São Paulo",
+          "language_pt-br": "pt",
+          "text": "São Paulo",
+          "language": "pt"
+        },
+        {
+          "id": "region.7229470134838220",
+          "wikidata": "Q175",
+          "short_code": "BR-SP",
+          "text_pt-br": "São Paulo",
+          "language_pt-br": "pt",
+          "text": "São Paulo",
+          "language": "pt"
+        },
+        {
+          "id": "country.9531777110682710",
+          "short_code": "br",
+          "wikidata": "Q155",
+          "text_pt-br": "Brasil",
+          "language_pt-br": "pt",
+          "text": "Brasil",
+          "language": "pt"
+        }
+      ]
+    },
+    {
+      "id": "address.1497656236",
+      "type": "Feature",
+      "place_type": [
+        "address"
+      ],
+      "relevance": 0.503333,
+      "properties": {
+        "accuracy": "street"
+      },
+      "text_pt-br": "Rua Turiassu",
+      "place_name_pt-br": "Rua Turiassu, Taquarussu, Campo Grande - Mato Grosso do Sul, 79006, Brasil",
+      "text": "Rua Turiassu",
+      "place_name": "Rua Turiassu, Taquarussu, Campo Grande - Mato Grosso do Sul, 79006, Brasil",
+      "center": [
+        -54.633898,
+        -20.483569
+      ],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -54.633898,
+          -20.483569
+        ]
+      },
+      "context": [
+        {
+          "id": "neighborhood.7485313557094560",
+          "text_pt-br": "Taquarussu",
+          "text": "Taquarussu"
+        },
+        {
+          "id": "postcode.7373096595782560",
+          "text_pt-br": "79006",
+          "text": "79006"
+        },
+        {
+          "id": "place.12120355757450440",
+          "wikidata": "Q168929",
+          "text_pt-br": "Campo Grande",
+          "language_pt-br": "pt",
+          "text": "Campo Grande",
+          "language": "pt"
+        },
+        {
+          "id": "region.14098898820945750",
+          "wikidata": "Q43319",
+          "short_code": "BR-MS",
+          "text_pt-br": "Mato Grosso do Sul",
+          "language_pt-br": "pt",
+          "text": "Mato Grosso do Sul",
+          "language": "pt"
+        },
+        {
+          "id": "country.9531777110682710",
+          "short_code": "br",
+          "wikidata": "Q155",
+          "text_pt-br": "Brasil",
+          "language_pt-br": "pt",
+          "text": "Brasil",
+          "language": "pt"
+        }
+      ]
+    },
+    {
+      "id": "address.360216164",
+      "type": "Feature",
+      "place_type": [
+        "address"
+      ],
+      "relevance": 0.503333,
+      "properties": {
+        "accuracy": "street"
+      },
+      "text_pt-br": "Rua Turiassu",
+      "place_name_pt-br": "Rua Turiassu Várzea Paulista - São Paulo, 13225, Brasil",
+      "text": "Rua Turiassu",
+      "place_name": "Rua Turiassu Várzea Paulista - São Paulo, 13225, Brasil",
+      "center": [
+        -46.842938,
+        -23.222025
+      ],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -46.842938,
+          -23.222025
+        ]
+      },
+      "context": [
+        {
+          "id": "postcode.9022652062781070",
+          "text_pt-br": "13225",
+          "text": "13225"
+        },
+        {
+          "id": "place.14036293686821320",
+          "wikidata": "Q1759112",
+          "text_pt-br": "Várzea Paulista",
+          "language_pt-br": "pt",
+          "text": "Várzea Paulista",
+          "language": "pt"
+        },
+        {
+          "id": "region.7229470134838220",
+          "wikidata": "Q175",
+          "short_code": "BR-SP",
+          "text_pt-br": "São Paulo",
+          "language_pt-br": "pt",
+          "text": "São Paulo",
+          "language": "pt"
+        },
+        {
+          "id": "country.9531777110682710",
+          "short_code": "br",
+          "wikidata": "Q155",
+          "text_pt-br": "Brasil",
+          "language_pt-br": "pt",
+          "text": "Brasil",
+          "language": "pt"
+        }
+      ]
+    },
+    {
+      "id": "address.1185449648",
+      "type": "Feature",
+      "place_type": [
+        "address"
+      ],
+      "relevance": 0.503333,
+      "properties": {
+        "accuracy": "street"
+      },
+      "text_pt-br": "Rua Turiassú",
+      "place_name_pt-br": "Rua Turiassú, Parque Pirajussara, Embu das Artes - São Paulo, 06815, Brasil",
+      "text": "Rua Turiassú",
+      "place_name": "Rua Turiassú, Parque Pirajussara, Embu das Artes - São Paulo, 06815, Brasil",
+      "center": [
+        -46.815764,
+        -23.653038
+      ],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -46.815764,
+          -23.653038
+        ]
+      },
+      "context": [
+        {
+          "id": "neighborhood.2922000143970870",
+          "text_pt-br": "Parque Pirajussara",
+          "text": "Parque Pirajussara"
+        },
+        {
+          "id": "locality.11019472148849890",
+          "text_pt-br": "Embu",
+          "text": "Embu"
+        },
+        {
+          "id": "postcode.3694504308698170",
+          "text_pt-br": "06815",
+          "text": "06815"
+        },
+        {
+          "id": "place.19999903879916400",
+          "wikidata": "Q651860",
+          "text_pt-br": "Embu das Artes",
+          "language_pt-br": "pt",
+          "text": "Embu das Artes",
+          "language": "pt"
+        },
+        {
+          "id": "region.7229470134838220",
+          "wikidata": "Q175",
+          "short_code": "BR-SP",
+          "text_pt-br": "São Paulo",
+          "language_pt-br": "pt",
+          "text": "São Paulo",
+          "language": "pt"
+        },
+        {
+          "id": "country.9531777110682710",
+          "short_code": "br",
+          "wikidata": "Q155",
+          "text_pt-br": "Brasil",
+          "language_pt-br": "pt",
+          "text": "Brasil",
+          "language": "pt"
+        }
+      ]
+    },
+    {
+      "id": "address.1709849079",
+      "type": "Feature",
+      "place_type": [
+        "address"
+      ],
+      "relevance": 0.503333,
+      "properties": {
+        "accuracy": "street"
+      },
+      "text_pt-br": "Rua Turiassu",
+      "place_name_pt-br": "Rua Turiassu São Paulo - São Paulo, 05005, Brasil",
+      "text": "Rua Turiassu",
+      "place_name": "Rua Turiassu São Paulo - São Paulo, 05005, Brasil",
+      "center": [
+        -46.681413,
+        -23.527129
+      ],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -46.681413,
+          -23.527129
+        ]
+      },
+      "context": [
+        {
+          "id": "locality.16135275663286880",
+          "wikidata": "Q10348536",
+          "text_pt-br": "Perdizes",
+          "language_pt-br": "pt",
+          "text": "Perdizes",
+          "language": "pt"
+        },
+        {
+          "id": "postcode.14659260658953980",
+          "text_pt-br": "05005",
+          "text": "05005"
+        },
+        {
+          "id": "place.9128493891838220",
+          "wikidata": "Q174",
+          "text_pt-br": "São Paulo",
+          "language_pt-br": "pt",
+          "text": "São Paulo",
+          "language": "pt"
+        },
+        {
+          "id": "region.7229470134838220",
+          "wikidata": "Q175",
+          "short_code": "BR-SP",
+          "text_pt-br": "São Paulo",
+          "language_pt-br": "pt",
+          "text": "São Paulo",
+          "language": "pt"
+        },
+        {
+          "id": "country.9531777110682710",
+          "short_code": "br",
+          "wikidata": "Q155",
+          "text_pt-br": "Brasil",
+          "language_pt-br": "pt",
+          "text": "Brasil",
+          "language": "pt"
+        }
+      ]
+    }
+  ],
+  "attribution": "NOTICE: © 2020 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained. POI(s) provided by Foursquare."
+}
+
 const app = {
 
   element : document.querySelector( '.app' ),
@@ -60,7 +405,13 @@ const app = {
 
         app.search.form.element.addEventListener( 'submit', function( event ) {
 
-          app.search.input.identify()
+          let suggestion = document.querySelector( '.suggestions ol li:first-child button' )
+
+          if ( suggestion )
+            suggestion.click()
+          else
+            app.search.input.identify()
+
           event.preventDefault()
 
         } )
@@ -99,8 +450,14 @@ const app = {
 
         let address = app.search.input.sanitized()
 
-        if ( address )
+        if ( address ) {
+
           console.log( 'Call MapBox API to get suggestions for: ' + address )
+
+          // if results:
+          app.search.suggestions.fill()
+
+        }
 
       },
 
@@ -135,21 +492,107 @@ const app = {
 
     suggestions : {
 
-      show : function() {
+      features : [
+        {
+          primary : 'Rua Olímpia de Almeida Prado',
+          secondary : 'Perdizes, São Paulo, SP — 05005',
+          center : [
+            -46.675731,
+            -23.530654
+          ]
+        },
+        {
+          primary : 'Rua Olimpio de Almeida Caxambu',
+          secondary : 'Perdizes, São Paulo, SP — 05005',
+          center : [
+            -46.675731,
+            -23.530654
+          ]
+        },
+        {
+          primary : 'Rua Maria Olympia de Almeida Sorocaba',
+          secondary : 'Perdizes, São Paulo, SP — 05005',
+          center : [
+            -46.675731,
+            -23.530654
+          ]
+        },
+        {
+          primary : 'Rua Olímpio Rodrigues de Almeida Itapetininga dos Santos',
+          secondary : 'Perdizes, São Paulo, SP — 05005',
+          center : [
+            -46.675731,
+            -23.530654
+          ]
+        },
+        {
+          primary : 'Rua Coronel Olimpio de Almeida Passa Quatro',
+          secondary : 'Perdizes, São Paulo, SP — 05005',
+          center : [
+            -46.675731,
+            -23.530654
+          ]
+        },
+      ],
 
-      },
+      // show : function() {
+      //
+      // },
 
-      hide : function() {
-
-      },
+      // hide : function() {
+      //  app.element.dataset.search = 'blur'
+      // },
 
       fill : function() {
+
+        app.search.suggestions.clear()
+
+        app.element.dataset.search = 'suggestions'
+
+        let ol = document.querySelector( '.suggestions ol' )
+
+        for ( let feature of app.search.suggestions.features ) {
+
+          let item, button, primary, secondary
+
+          item = document.createElement( 'li' )
+
+          button = document.createElement( 'button' )
+          button.setAttribute( 'type', 'button' )
+          button.value = JSON.stringify( feature.center )
+
+          button.addEventListener( 'click', function() {
+
+            let center = JSON.parse( this.value )
+            app.story.begin( center )
+
+          } )
+
+          primary = document.createElement( 'span' )
+          primary.innerText = feature.primary
+
+          secondary = document.createElement( 'span' )
+          secondary.innerText = feature.secondary
+
+          button.appendChild( primary )
+          button.appendChild( secondary )
+          item.appendChild( button )
+          ol.appendChild( item )
+
+        }
 
       },
 
       clear : function() {
 
-        console.log( 'Clear suggestions' )
+        let ol = document.querySelector( '.suggestions ol' )
+
+        let item = ol.lastElementChild
+
+        while ( item ) {
+            ol.removeChild(item)
+            item = ol.lastElementChild
+        }
 
       },
 
@@ -161,16 +604,29 @@ const app = {
 
     geolocation : {
 
-      send : function( position ) {
+      handle : function( position ) {
 
-        alert( position.coords.latitude + ', ' + position.coords.longitude )
+        console.log( position )
+
+        if ( position.coords ) {
+
+          let center = [
+            position.coords.latitude,
+            position.coords.longitude
+          ]
+
+          alert( JSON.stringify( center ) )
+
+          app.story.begin( center )
+
+        }
 
       },
 
       get : function() {
 
         if ( navigator.geolocation )
-          navigator.geolocation.getCurrentPosition( app.search.geolocation.send )
+          navigator.geolocation.getCurrentPosition( app.search.geolocation.handle )
 
       },
 
@@ -202,9 +658,9 @@ const app = {
 
       map : {
 
-        center : function(  ) {
+        center : function( center ) {
 
-          console.log( 'Center map on: ' + latitude + ', ' + longitude )
+          console.log( 'Center map on: ' + center[ 0 ] + ', ' + center[ 1 ] )
 
         }
 
@@ -212,10 +668,16 @@ const app = {
 
     },
 
-    begin : function( latitude, longitude ) {
+    begin : function( center ) {
 
-      app.story.canvas.map.center( latitude, longitude )
+      app.search.suggestions.clear()
+      app.search.form.element.reset()
+      app.search.input.element.blur()
+
       app.pages.open( 'story' )
+
+      app.story.canvas.map.center( center )
+      app.story.carousel.instance.update()
 
     },
 
