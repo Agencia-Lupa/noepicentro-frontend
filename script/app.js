e474bcd4541f22442a19c2a4197eddb8a4d4af33
Vinicius Sueiro <hi@vsueiro.com>
2020-07-04 16:38:07 -0300

    Removes unnecessary nesting

--- script/app.js
+++ script/app.js
@@ -357,18 +357,11 @@ let app = {
       id : 'map',
       style : 'mapbox://styles/tiagombp/ckbz4zcsb2x3w1iqyc3y2eilr',
       token : 'pk.eyJ1IjoidGlhZ29tYnAiLCJhIjoiY2thdjJmajYzMHR1YzJ5b2huM2pscjdreCJ9.oT7nAiasQnIMjhUB-VFvmw',
-
-      user : {
-
-        center : undefined
-
-      },
+      user : undefined,
 
       initialize : function( center ) {
 
-        console.log( 'center', center )
-
-        app.story.map.user.center = center;
+        app.story.map.user = center;
 
         mapboxgl.accessToken = app.story.map.token;
 
@@ -449,7 +442,7 @@ let app = {
 
                     app.story.map.show_people();
                     app.story.map.highlight_people_inside(
-                      center = app.story.map.user.center,
+                      center = app.story.map.user,
                       point_on_circle = api_result[1]
                     );
 
