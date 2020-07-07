12590b77f98257ee9f458c552d2f2aeda1801718
Vinicius Sueiro <hi@vsueiro.com>
2020-07-07 16:59:38 -0300

    Tries to optimize MapBox

--- script/app.js
+++ script/app.js
@@ -850,7 +850,7 @@ let app = {
     map : {
 
       id : 'map',
-      style : 'mapbox://styles/tiagombp/ckbz4zcsb2x3w1iqyc3y2eilr',
+      style : 'mapbox://styles/tiagombp/ckbz4zcsb2x3w1iqyc3y2eilr?optimize=true',
       token : 'pk.eyJ1IjoidGlhZ29tYnAiLCJhIjoiY2thdjJmajYzMHR1YzJ5b2huM2pscjdreCJ9.oT7nAiasQnIMjhUB-VFvmw',
       user : undefined,
 
